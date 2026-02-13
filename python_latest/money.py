722f14a6aff9af20107054ea8e591f102966565b
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-02-12 19:33:04 -0500

    Rebuild the Money class and remove some unneeded behavior

--- python_latest/money.py
+++ python_latest/money.py
@@ -1,18 +1,18 @@
-"""Exact dollar amounts implemented as a wrapper around an int value.
+"""Exact dollar amounts as a dataclass wrapper around an int value."""
 
-Last modified 26 Jun 2023 by Greg Vance.
-"""
-
-
+import dataclasses
 import re
-from typing import Any, Dict, Union
+from typing import Any, Final, Self
 
 from comma_int import parse_comma_int, render_comma_int
 
 
-DOLLAR_CENTS = 100  # Number of cents that make up one dollar
+# The number of cents that make up one dollar
+DOLLAR_CENTS: Final[int] = 100
 
-MONEY_FORMAT_REGEX = re.compile(r"""
+
+# Regex to match the format of money strings
+MONEY_FORMAT_REGEX: Final[re.Pattern[str]] = re.compile(r"""
     \A\s*               # Optional whitespace at start
     (?P<sign>[-+]?)     # then an optional +/- sign
     \$?                 # and an optional $ before any digits
@@ -29,21 +29,26 @@ MONEY_FORMAT_REGEX = re.compile(r"""
 """, re.VERBOSE)
 
 
+@dataclasses.dataclass(
+    repr=False,
+    order=True,
+    frozen=True,
+    match_args=False,
+    slots=True,
+)
 class Money:
-    """Nicely represents an amount of money in dollars and cents exactly. The
-    internally stored value is an exact integer number of cents. Other features
-    include nice parsing and printing of monetary strings.
-    """
+    """Nicely represents an amount of money in dollars and cents exactly.
 
-    __slots__ = ("_cents",)
+    The internally stored value is an exact integer number of cents. Other
+    features include nice parsing and printing of monetary strings.
+    """
+    cents: int
 
     # Constructors
 
-    def __init__(self, cents: int) -> None:
-        self._cents = cents
-
     @classmethod
-    def parse(cls, s: str) -> 'Money':
+    def parse(cls, s: str) -> Self:
+        """Parse a Money object from a string representation."""
 
         regex_match = MONEY_FORMAT_REGEX.fullmatch(s)
         if not regex_match:
@@ -65,34 +70,15 @@ class Money:
         cents_string = regex_match.group("cents").ljust(2, '0')
         cents = int(cents_string)
 
-        return Money(sign * (dollars * DOLLAR_CENTS + cents))
+        return cls(sign * (dollars * DOLLAR_CENTS + cents))
 
     @classmethod
-    def from_numeric(cls, n: float, picky: bool = True) -> 'Money':
-        """Interpret a numeric type as Money by rounding to the nearest cent.
-
-        Construct a new Money object whose value in dollars and cents is given
-        by n, which can be a float or an int. If picky is True (default
-        behavior), then the input *must* be as near as possible to an exact
-        multiple of 1 cent, such that float(Money.from_numeric(n)) == n. If
-        picky is False, then the input is simply rounded to the nearest cent
-        without regard for whether the conversion is reversible.
-        """
-        # The round() function returns an int when ndigits is omitted
-        money = Money(round(n * DOLLAR_CENTS))
-
-        if not picky or float(money) == n:
-            return money
-        else:
-            raise ValueError(f"cannot convert {n!r} to Money with picky=True")
-
-    @classmethod
-    def deserialize(cls, obj: Dict[str, Any]) -> 'Money':
+    def deserialize(cls, obj: dict[str, Any]) -> Self:
         """Reconstruct Money which was serialized to a JSON object (dict)."""
         assert set(obj.keys()) == {"class", "cents"}
         assert obj["class"] == "Money"
         assert isinstance(obj["cents"], int)
-        return Money(obj["cents"])
+        return cls(obj["cents"])
 
     # Output methods
 
@@ -103,137 +89,65 @@ class Money:
         return f"{sign_string}${dollars_string}.{cents_string}"
 
     def __repr__(self) -> str:
-        return f"Money({self._cents})"
+        return f"Money({self.cents})"
 
-    def serialize(self) -> Dict[str, Any]:
+    def serialize(self) -> dict[str, Any]:
         """Convert Money to a dict (object) that JSON can comprehend."""
-        return {"class": "Money", "cents": self._cents}
+        return {"class": "Money", "cents": self.cents}
 
     # Properties for controlled access to internals
 
     @property
     def sign(self) -> int:
-        if self._cents > 0:
+        if self.cents > 0:
             return +1
-        elif self._cents < 0:
+        if self.cents < 0:
             return -1
-        else:
-            return 0
+        return 0
 
     @property
     def unsigned_dollars(self) -> int:
-        return abs(self._cents) // DOLLAR_CENTS
+        return abs(self.cents) // DOLLAR_CENTS
 
     @property
     def unsigned_cents(self) -> int:
-        return abs(self._cents) % DOLLAR_CENTS
+        return abs(self.cents) % DOLLAR_CENTS
 
     @property
     def in_pennies(self) -> int:
-        return self._cents
-
-    # Conversions to Python numeric types
-
-    def __float__(self) -> float:
-        return self._cents / float(DOLLAR_CENTS)
-
-    def __int__(self) -> int:
-        if self._cents >= 0:
-            # Floor positive numbers towards zero
-            return self._cents // DOLLAR_CENTS
-        else:
-            # Ceil negative numbers towards zero
-            return -(-self._cents // DOLLAR_CENTS)
-
-    def __bool__(self) -> bool:
-        return self._cents != 0
-
-    # Comparisons
-
-    def _compare(self, other: Union['Money', float]) -> int:
-        # Basically an old C-style cmp() function for doing comparisons
-        # Also handles any conversions to Money from other numeric types
-        if isinstance(other, Money):
-            return self._cents - other._cents
-        else:
-            # Note: the setting picky=True right here is essential for making
-            # the Money class hashable according to Python's hashing rules.
-            other_as_money = Money.from_numeric(other, picky=True)
-            return self._cents - other_as_money._cents
-
-    def __lt__(self, other: Union['Money', float]) -> bool:
-        return self._compare(other) < 0
-
-    def __le__(self, other: Union['Money', float]) -> bool:
-        return self._compare(other) <= 0
-
-    def __eq__(self, other: Union['Money', float]) -> bool:
-        return self._compare(other) == 0
-
-    def __ne__(self, other: Union['Money', float]) -> bool:
-        return self._compare(other) != 0
-
-    def __gt__(self, other: Union['Money', float]) -> bool:
-        return self._compare(other) > 0
-
-    def __ge__(self, other: Union['Money', float]) -> bool:
-        return self._compare(other) >= 0
+        return self.cents
 
     # Unary math operations
 
-    def __pos__(self) -> 'Money':
-        return Money(+self._cents)
+    def __neg__(self) -> Self:
+        return self.__class__(-self.cents)
 
-    def __neg__(self) -> 'Money':
-        return Money(-self._cents)
-
-    def __abs__(self) -> 'Money':
-        return Money(abs(self._cents))
+    def __abs__(self) -> Self:
+        return self.__class__(abs(self.cents))
 
     # Binary arithmetic operations
 
-    def __add__(self, other: Union['Money', float]) -> 'Money':
-        if isinstance(other, Money):
-            return Money(self._cents + other._cents)
-        else:
-            other_as_money = Money.from_numeric(other, picky=False)
-            return Money(self._cents + other_as_money._cents)
+    def __add__(self, other: Self) -> Self:
+        return self.__class__(self.cents + other.cents)
 
-    def __sub__(self, other: Union['Money', float]) -> 'Money':
+    def __sub__(self, other: Self) -> Self:
         return self + -other
 
-    def __mul__(self, other: int) -> 'Money':
-        return Money(self._cents * other)
+    def __mul__(self, other: int) -> Self:
+        return self.__class__(self.cents * other)
 
     # Reflected binary arithmetic operations
 
-    def __radd__(self, other: float) -> 'Money':
-        return self + other
-
-    def __rsub__(self, other: float) -> 'Money':
-        return -self + other
-
-    def __rmul__(self, other: int) -> 'Money':
+    def __rmul__(self, other: int) -> Self:
         return self * other
 
     # Augmented assignment
 
-    def __iadd__(self, other: Union['Money', float]) -> 'Money':
+    def __iadd__(self, other: Self) -> Self:
         return self + other
 
-    def __isub__(self, other: Union['Money', float]) -> 'Money':
+    def __isub__(self, other: Self) -> Self:
         return self - other
 
-    def __imul__(self, other: int) -> 'Money':
+    def __imul__(self, other: int) -> Self:
         return self * other
-
-    # Other methods
-
-    def __hash__(self) -> int:
-        # Objects that compare equal in Python must have the same hash value.
-        # Since Money objects can be compared with floats and ints, we want to
-        # hash float(self) in this method to ensure that holds. This has the
-        # potential for all sorts of contradictions, which are prevented by the
-        # use of Money.from_numeric() with the picky=True setting inside the
-        # _compare() method.
-        return hash(float(self))
